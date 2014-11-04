40a35d773f5f1738f031a3592017ff6c949c77d2
Weslley Araujo <weslleyaraujo20@gmail.com>
2014-11-03 21:44:37 -0500

    done :D

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,14 @@
+module.exports = function toRna(dna) {
+  var conversion = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U'
+  };
+
+  dna = dna || '';
+
+  return dna.split('').map(function (item) {
+    return conversion[item];
+  }).join('');
+};
