9042a59ee1fa11e8f919b47dfe3852dc0fb669a1
Weslley Araujo <weslleyaraujo20@gmail.com>
2018-03-28 16:18:43 +0200

    feat(gigaseconds): solution

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,3 +1,11 @@
-class Gigasecond {}
+class Gigasecond {
+  constructor(value) {
+    this.value = value;
+  }
+
+  date() {
+    return new Date(this.value.getTime() + 1000000000000);
+  }
+}
 
 module.exports = Gigasecond;
